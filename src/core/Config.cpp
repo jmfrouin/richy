76c29465528a516fc9d0091d9a8d76aba9e839db
Jean-Michel Frouin <jm@frouin.me>
2025-07-09 21:50:25 +0200

    Update build number to 8, add core module with basic structure, and link it to the main executable.

---
+++ src/core/Config.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jean-Michel Frouin on 09/07/2025.
+//
+
+#include "Config.h"
---
+++ src/core/Config.h
@@ -0,0 +1,16 @@
+//
+// Created by Jean-Michel Frouin on 09/07/2025.
+//
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+
+
+class Config {
+
+};
+
+
+
+#endif //CONFIG_H
